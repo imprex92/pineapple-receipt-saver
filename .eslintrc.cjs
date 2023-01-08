bdfc921e58066957009954940ea7e2f476171f9e
Daniel Bréelle Johannesson <daniel@johannesson.cz>
2023-01-08 21:55:18 +0100

    Added settings + Absoulte paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,12 @@
 module.exports = {
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.d.ts', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
